e88f7b510bef4d57d37a2007624f4d5ff10ddf04
Harriemburu <40524846+Harrieson@users.noreply.github.com>
2024-04-21 13:44:29 +0300

    Posts Controller

---
+++ backend/controllers/post.controller.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+
+	"github.com/Harrieson/golangbackend/database"
+	"github.com/Harrieson/golangbackend/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func CreatePost(c *fiber.Ctx) error {
+	var blogpost models.Blog
+	if err := c.BodyParser(&blogpost); err != nil {
+		fmt.Println("Unable to parse Body")
+	}
+
+	if err := database.DB.Create(&blogpost).Error; err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Invalid payload, Please check and post again.",
+		})
+	}
+	fmt.Println(&blogpost)
+	return c.JSON(fiber.Map{
+		"message": "Congratulation, Your post is now published",
+	})
+}
+
+func GetAllPosts(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 10
+	offset := (page - 1) * limit
+
+	var total int64
+	var getBlog []models.Blog
+	database.DB.Preload("User").Offset(offset).Limit(limit).Find(&getBlog)
+	database.DB.Model(&models.Blog{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"data": getBlog,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
+}
+
+func DetailPost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var blogpost models.Blog
+
+	database.DB.Where("id=?", id).Preload("User").First(&blogpost)
+
+	return c.JSON(fiber.Map{
+		"data": blogpost,
+	})
+}
